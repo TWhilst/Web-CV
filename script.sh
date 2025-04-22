575d424ca784b5e8378bb52aa5e64793d80ee89b
TWhilst <tochethestrict@gmail.com>
2025-04-22 19:00:26 +0100

    made changes to index.html and script.sh

--- script.sh
+++ script.sh
@@ -6,10 +6,12 @@ sudo su -
 aws s3 mb s3://toche-portfolio-ami-0e449927258d45bc4
 yum install -y git
 git clone https://github.com/TWhilst/Web-CV.git
-aws s3 cp Web-CV/profile.png s3://toche-portfolio-ami-0e449927258d45bc4
-aws s3 cp Web-CV/style.css s3://toche-portfolio-ami-0e449927258d45bc4
-aws s3 cp Web-CV/index.html s3://toche-portfolio-ami-0e449927258d45bc4
+cd Web-CV
+aws s3 cp profile.png s3://toche-portfolio-ami-0e449927258d45bc4
+aws s3 cp style.css s3://toche-portfolio-ami-0e449927258d45bc4
+aws s3 cp index.html s3://toche-portfolio-ami-0e449927258d45bc4
 aws s3api put-public-access-block --bucket toche-portfolio-ami-0e449927258d45bc4 --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
 aws s3api put-bucket-policy --bucket toche-portfolio-ami-0e449927258d45bc4 --policy file://policy.json
 aws s3 website s3://portfolio1-ami-0e449927258d45bc4 --index-document index.html
+aws s3api put-bucket-lifecycle --bucket toche-portfolio-ami-0e449927258d45bc4 --lifecycle-configuration file://lifecycle-conf.json
 fi
